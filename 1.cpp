b2314050121e25fa1f191788e1e981205bef647e
Krishan Kant <krishankant882005@gmail.com>
2026-09-19 23:31:22 +0530

    Array traversal

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+// Array Traversal
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {12,24,85,98,45};
+    int n = 5;
+    for(int i = 0; i <n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
