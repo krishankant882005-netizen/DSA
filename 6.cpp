c3afb9ec54a725867ec718c62c78f2b7b98664a0
Krishan Kant <krishankant882005@gmail.com>
2026-09-23 02:05:12 +0530

    Insertion Sort

--- 6.cpp
+++ 6.cpp
@@ -1,18 +1,19 @@
+// Bubble Sort
 #include <iostream>
 using namespace std;
 int main() {
-    int arr[] = {3,2,8,4,9,1,5,7};
+    int arr[] = {3,1,5,9,4,0,2,8};
     int n = 8;
-    for(int i = 0; i < n - 1; i++){
-        for(int j= 0; j < n - i - 1; j++) {
-            if(arr[j] > arr[j+1]){
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j+1]) {
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
             }
         }
     }
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; n++) {
         cout << arr[i] << " ";
     }
     return 0;
---
+++ 7.cpp
@@ -0,0 +1,20 @@
+// Insertion Sort
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {4,1,8,5,0,2};
+    int n = 6;
+    for(int i = 1; i < n; i++) {
+         int key =  arr[i];
+         int j = i - 1;
+         while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+         }
+         arr[j + 1] = key;
+    }
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
